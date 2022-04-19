67d2fb7bbd786f647c2d0ee3d988f790df9d0787
santiago chinellato <santiagochinellato@gmail.com>
2022-04-19 13:08:59 -0300

    se agregó las sección team

--- src/components/AwesomeTeam.js
+++ src/components/AwesomeTeam.js
@@ -3,8 +3,48 @@ import AwesomeTeamCards from "./AwesomeTeamCards/AwesomeTeamCards";
 
 const AwesomeTeam = () => {
   return (
-    <div>
-      <AwesomeTeamCards />
+    <div className="awesomeTeam">
+      <img className="separador" src="./images/separador.png" alt="" />
+      <h2 className="Titulo">Awesome Team</h2>
+      <p className="subtituloSections">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ducimus
+        aliquam sit et labore soluta?
+      </p>
+      <div className="contenedorTeamCards">
+        <AwesomeTeamCards nombre="SUNNY KHAN" cargo="CEO" imagen="one" />
+        <AwesomeTeamCards
+          nombre="AJOY DAS"
+          cargo="BUSINESS DEVELOPMENT"
+          imagen="two"
+        />
+        <AwesomeTeamCards
+          nombre="AFROZA MOU"
+          cargo="UX/UI DESIGNER"
+          imagen="three"
+        />
+        <AwesomeTeamCards
+          nombre="LIM SARAH"
+          cargo="HEAD OF MARKETING"
+          imagen="four"
+        />
+        <AwesomeTeamCards nombre="LIM SARAH" cargo="CEO" imagen="one" />
+
+        <AwesomeTeamCards
+          nombre="SUNNY KHAN"
+          cargo="HEAD OF MARKETING"
+          imagen="four"
+        />
+        <AwesomeTeamCards
+          nombre="AJOY DAS"
+          cargo="UX/UI DESIGNER"
+          imagen="two"
+        />
+        <AwesomeTeamCards
+          nombre="AFROZA MOU"
+          cargo="BUSINESS DEVELOPMENT"
+          imagen="three"
+        />
+      </div>
     </div>
   );
 };
--- src/components/AwesomeTeamCards/AwesomeTeamCards.js
+++ src/components/AwesomeTeamCards/AwesomeTeamCards.js
@@ -1,11 +1,21 @@
 import React from "react";
-
-const AwesomeTeamCards = () => {
+import { BsTwitter, BsInstagram, BsFacebook, BsSkype } from "react-icons/bs";
+const AwesomeTeamCards = (props) => {
   return (
-    <div>
-      <img src="" alt="" />
-      <h6>Lorem, ipsum.</h6>
-      <p>Lorem, ipsum dolor.</p>
+    <div className="cardTeam">
+      <img
+        className="imageTeam"
+        src={`./images/pp_${props.imagen}.png`}
+        alt=""
+      />
+      <h6 className="titleTeam">{props.nombre}</h6>
+      <p className="textTeam">{props.cargo}</p>
+      <div className="containerRS">
+        <BsTwitter className="iconTeam" />
+        <BsInstagram className="iconTeam" />
+        <BsFacebook className="iconTeam" />
+        <BsSkype className="iconTeam" />
+      </div>
     </div>
   );
 };
